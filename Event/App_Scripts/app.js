4b7823622630088f2377d50ae259cab06424552f
InfoSphereSolution <InfoSphereSolution@gmail.com>
2016-12-13 19:57:04 +0530

    Added index and other pages

---
+++ Event/App_Scripts/app.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="../Scripts/angular.min.js" />
+/// <reference path="../Scripts/angular-ui-router.min.js" />
+
+
+var app = angular.module("eventApp", ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("#");
+    $stateProvider
+         .state('home', {
+             url: "/Home.html",
+             templateUrl: "Pages/HomeContent.html"
+         })
+
+        .state('services', {
+            url: "Services",
+            templateUrl: "Pages/Services.html"
+        })
+
+        .state('photos', {
+         url: "Photos",
+         templateUrl: "Pages/Photos.html"
+        })
+
+        .state('videos', {
+         url: "Videos",
+         templateUrl: "Pages/Videos.html"
+        })
+
+        .state('aboutus', {
+         url: "AboutUs",
+         templateUrl: "Pages/AboutUs.html"
+        })
+
+        .state('contactus', {
+        url: "ContactUs",
+        templateUrl: "Pages/ContactUs.html"
+        })
+});
