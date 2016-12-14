2d32123ca82feba9d80cd571e7cbd9f429467881
InfoSphereSolution <InfoSphereSolution@gmail.com>
2016-12-14 13:57:05 +0530

    updated home content

--- Event/App_Scripts/app.js
+++ Event/App_Scripts/app.js
@@ -4,35 +4,35 @@
 
 var app = angular.module("eventApp", ['ui.router']);
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("#");
+    $urlRouterProvider.otherwise('/Home');
     $stateProvider
          .state('home', {
-             url: "/Home.html",
+             url: "/Home",
              templateUrl: "Pages/HomeContent.html"
          })
 
         .state('services', {
-            url: "Services",
-            templateUrl: "Pages/Services.html"
+            url: '/Services',
+            templateUrl: 'Pages/Services.html'
         })
 
         .state('photos', {
-         url: "Photos",
-         templateUrl: "Pages/Photos.html"
+         url: '/Photos',
+         templateUrl: 'Pages/Photos.html'
         })
 
         .state('videos', {
-         url: "Videos",
-         templateUrl: "Pages/Videos.html"
+         url: '/Videos',
+         templateUrl: 'Pages/Videos.html'
         })
 
         .state('aboutus', {
-         url: "AboutUs",
-         templateUrl: "Pages/AboutUs.html"
+         url: '/AboutUs',
+         templateUrl: 'Pages/AboutUs.html'
         })
 
         .state('contactus', {
-        url: "ContactUs",
-        templateUrl: "Pages/ContactUs.html"
+        url: '/ContactUs',
+        templateUrl: 'Pages/ContactUs.html'
         })
 });
